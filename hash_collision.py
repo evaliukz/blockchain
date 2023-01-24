d89237d759fb0723b7aa5d031d89a7093318774f
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-01-23 18:46:47 -0800

    Add files via upload

--- hash_collision.py
+++ hash_collision.py
@@ -1,29 +1,44 @@
 import hashlib
-import string
+import os
 import random
+import string
+
+def generateRandomString():
+    letters = string.ascii_letters
+    string = ''.join(random.choice(letters) for i in range(10))
+    return string
+
 
 def hash_collision(k):
-    if not isinstance(k,int):
-        print( "hash_collision expects an integer" )
-        return( b'\x00',b'\x00' )
+    # check k
+    if not isinstance(k, int):
+        print("hash_collision expects an integer")
+        return (b'\x00', b'\x00')
     if k < 0:
-        print( "Specify a positive number of bits" )
-        return( b'\x00',b'\x00' )
-   
-    #Collision finding code goes here
-    ascii_letters = string.ascii_letters
-    x = ''.join(random.choice(ascii_letters) for i in range(256)).encode('utf-8')
-    y = ''.join(random.choice(ascii_letters) for i in range(256)).encode('utf-8')
-    
-    isCollision = 0
-    while (isCollision == 0):
-        shaX = hashlib.sha256(x).digest()
-        shaY = hashlib.sha256(y).digest()
-        binaryX = bin(int.from_bytes(shaX, 'cat'))
-        binaryY = bin(int.from_bytes(shaY, 'cat'))
-        if (binaryX[(len(binaryX)-k):] == binaryY[(len(binaryY)-k):]):
-            isCollision = 1
-        else:
-            strY = ''.join(random.choice(ascii_letters) for i in range (256))
-            y = strY.encode('utf-8')
+        print("Specify a positive number of bits")
+        return (b'\x00', b'\x00')
+
+    # generate a random string, convert it bytecode and assign it to x
+    x = generateRandomString().encode('utf-8')
+
+    # get the sha256 hash value of the byte code
+    hex_result_x_random = hashlib.sha256(x).hexdigest()
+
+    bin_result_x_random = (bin(int(hex_result_x_random, 16))[2:]).zfill(256)
+
+    # use a while loop to find y with brute force
+    while True:
+        # generate a random string
+        # convert the str to bytecode and assign it to x
+        y = generateRandomString().encode('utf-8')
+
+        # get the sha256 hash value of the byte code
+        hex_result_y_random = hashlib.sha256(y).hexdigest()
+
+        bin_result_y_random = (bin(int(hex_result_y_random, 16))[2:]).zfill(256)
+
+        # compare the last k digits
+        if (bin_result_y_random[-k:] == bin_result_x_random[-k:]) :
+            break
+
     return (x, y)
