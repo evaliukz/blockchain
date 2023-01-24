042bd179f573d59ac90eeca2f270ebc696b1aebc
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-01-23 19:20:37 -0800

    Add files via upload

--- hash_preimage.py
+++ hash_preimage.py
@@ -1,24 +1,23 @@
 import hashlib
-import string
+import os
 import random
+import string
+
 
 def hash_preimage(target_string):
-    if not all( [x in '01' for x in target_string ] ):
-        print( "Input should be a string of bits" )
+    if not all([x in '01' for x in target_string]):
+        print("Input should be a string of bits")
         return
-    nonce = b'\x00'
+    x = b'\x00'
+    # target_bit = target_string.encode('utf-8')
+    k = len(target_string)
+    x = random.choice(string.ascii_letters)
+    x_sha = hashlib.sha256(x.encode('utf-8'))
+    final_k_digits_x = bin(int(x_sha.hexdigest(), base=16))[-k:]
 
-    length = len(target_string)
-    
-    # create a random string
-    random_string = random.choice(string.ascii_letters)
-    # get sha256 value and convert to binary
-    random_string_sha = hashlib.sha256(random_string.encode('utf-8')) # convert the string to byte
-    random_string_sha_binary = bin(int(random_string_sha .hexdigest(), base=16))[-length:]
-    # compare string_sha_binary with target_string byte
-    while random_string_sha_binary != target_string:
-        nonce += random.choice(string.ascii_letters)
-        random_string_sha = hashlib.sha256(nonce.encode('utf-8'))
-        random_string_sha_binary = bin(int(random_string_sha.hexdigest(), base=16))[-length:]
+    while final_k_digits_x != target_string:
+        x += random.choice(string.ascii_letters)
+        x_sha = hashlib.sha256(x.encode('utf-8'))
+        final_k_digits_x = bin(int(x_sha.hexdigest(), base=16))[-k:]
 
-    return nonce
+    return x
