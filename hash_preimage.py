9e4a6d25074d4728b2411d737b326effb7cc93b1
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-01-23 19:25:55 -0800

    Add files via upload

--- hash_preimage.py
+++ hash_preimage.py
@@ -8,16 +8,16 @@ def hash_preimage(target_string):
     if not all([x in '01' for x in target_string]):
         print("Input should be a string of bits")
         return
-    x = b'\x00'
-    # target_bit = target_string.encode('utf-8')
+    nonce = b'\x00'
+   
     k = len(target_string)
-    x = random.choice(string.ascii_letters)
-    x_sha = hashlib.sha256(x.encode('utf-8'))
-    final_k_digits_x = bin(int(x_sha.hexdigest(), base=16))[-k:]
+    nonce = random.choice(string.ascii_letters) 
+    nonce_sha = hashlib.sha256(nonce.encode('utf-8')) #convert to byte and get sha
+    nonce_sha_binary = bin(int(nonce_sha.hexdigest(), base=16))[-k:] #to binary
 
-    while final_k_digits_x != target_string:
-        x += random.choice(string.ascii_letters)
-        x_sha = hashlib.sha256(x.encode('utf-8'))
-        final_k_digits_x = bin(int(x_sha.hexdigest(), base=16))[-k:]
+    while nonce_sha_binary != target_string: #compare the random to target string
+        nonce += random.choice(string.ascii_letters)
+        nonce_sha = hashlib.sha256(nonce.encode('utf-8'))  #convert to byte and get sha
+        nonce_sha_binary = bin(int(nonce_sha.hexdigest(), base=16))[-k:]  #to binary
 
-    return x
+    return nonce
