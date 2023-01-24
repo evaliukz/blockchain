06edb4887e0b6ef2a92397232f3eaf9fc7b26536
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-01-23 19:19:08 -0800

    Add files via upload

--- hash_preimage.py
+++ hash_preimage.py
@@ -8,19 +8,17 @@ def hash_preimage(target_string):
         return
     nonce = b'\x00'
 
-    len = len(target_string)
-    ascii_letters = string.ascii_letters
+    length = len(target_string)
+    
+    # create a random string
+    random_string = random.choice(string.ascii_letters)
+    # get sha256 value and convert to binary
+    random_string_sha = hashlib.sha256(random_string.encode('utf-8')) # convert the string to byte
+    random_string_sha_binary = bin(int(random_string_sha .hexdigest(), base=16))[-length:]
+    # compare string_sha_binary with target_string byte
+    while random_string_sha_binary != target_string:
+        nonce += random.choice(string.ascii_letters)
+        random_string_sha = hashlib.sha256(nonce.encode('utf-8'))
+        random_string_sha_binary = bin(int(random_string_sha.hexdigest(), base=16))[-length:]
 
-    while True:
-        # create a random string
-        random_string = ''.join(random.choice(ascii_letters) for i in range(10))
-        # get sha256 value and convert to binary
-        random_string_sha = hashlib.sha256(random_string.encode('utf-8')) # convert the string to byte
-        random_string_sha_hex = random_string_sha.hexdigest()
-        random_string_sha_binary = (bin(int(random_string_sha_hex, 16))[2:]).zfill(256)
-        # compare string_sha_binary with target_string byte
-        if ( random_string_sha_binary[-len:] == target_string ):
-            nonce = random_string.encode('utf-8')
-            break
-
-    return( nonce )
+    return nonce
