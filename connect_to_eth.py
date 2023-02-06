abd78426e5330ca2187455f2d5e346d56acb8395
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-02-06 00:49:17 -0800

    Add files via upload

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -4,10 +4,10 @@ from web3.providers.rpc import HTTPProvider
 #If you use one of the suggested infrastructure providers, the url will be of the form
 #now_url  = f"https://eth.nownodes.io/{now_token}"
 #alchemy_url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
-#infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
+infura_url = f"https://mainnet.infura.io/v3/771f947bc18c42fe8d96a110ff930123"
 
 def connect_to_eth():
-	url = https://mainnet.infura.io/v3/771f947bc18c42fe8d96a110ff930123
+	url = infura_url
 	w3 = Web3(HTTPProvider(url))
 	assert w3.isConnected(), f"Failed to connect to provider at {url}"
 	return w3
