4b990a869816c18ddeceae87da6eb2edd79a11ad
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-04-03 18:54:55 -0700

    Add files via upload

--- send_tokens.py
+++ send_tokens.py
@@ -30,7 +30,7 @@ def send_tokens( receiver_pk, tx_amount ):
     sender_pk = mnemonic.to_public_key(mnemonic_secret)
 
     #create transaction
-    txn = transaction.PaymentTxn(sender_pk, tx_fee, first_valid_round, last_valid_round, gen_hash, receiver_pk, tx_amount, flat_fee=True)
+    txn = transaction.PaymentTxn(sender_pk, tx_fee, first_valid_round, last_valid_round, gen_hash, receiver_pk, tx_amount)
 
     # sign
     signed_tx = txn.sign(sk)
@@ -38,6 +38,7 @@ def send_tokens( receiver_pk, tx_amount ):
     # send
     txid = acl.send_transaction(signed_tx)
     wait_for_confirmation(acl, txid)
+    print("txid is ", txid)
 
     return sender_pk, txid
 
