e23f53797ffdc5bb9a325e4c3c845ee82ff79646
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-04-03 20:22:09 -0700

    Add files via upload

--- order_book.py
+++ order_book.py
@@ -9,6 +9,7 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 def process_order(order):
+    #Your code here
     new_order = Order(
         buy_currency=order['buy_currency'],
         sell_currency=order['sell_currency'],
@@ -20,38 +21,55 @@ def process_order(order):
     session.add(new_order)
     session.commit()
 
-    for old_order in session.query(Order).filter(Order.filled == None).all():
-            if old_order.sell_currency != new_order.buy_currency:
-                continue
-            if old_order.buy_currency != new_order.sell_currency:
-                continue
-            if old_order.sell_amount / old_order.buy_amount < new_order.buy_amount / new_order.sell_amount:
-                continue
-            old_order.filled = datetime.now()
-            new_order.filled = datetime.now()
-            old_order.counterparty_id = new_order.id
-            new_order.counterparty_id = old_order.id
-            session.commit()
-            
-    
-            if new_order.buy_amount > old_order.sell_amount:
-            
-                new = Order(sender_pk = new_order.sender_pk,receiver_pk = new_order.receiver_pk, 
-                                  buy_currency = new_order.buy_currency, sell_currency = new_order.sell_currency, 
-                                  buy_amount = new_order.buy_amount - old_order.sell_amount, 
-                                  sell_amount = (new_order.buy_amount - old_order.sell_amount)* new_order.sell_amount / new_order.buy_amount,
-                                  creator_id = new_order.id)
-                session.add(new)
-                session.commit()
 
-                                           
+    orders = session.query(Order).filter(Order.filled == None).all()
+    for existing_order in orders:
+        if ((existing_order.buy_currency != new_order.sell_currency)
+            or (existing_order.sell_currency != new_order.buy_currency)
+            or (existing_order.sell_amount / existing_order.buy_amount < new_order.buy_amount / new_order.sell_amount)
+            or (existing_order.counterparty_id != None)):
+            continue
+   
+        match_order = existing_order
+
+        match_order.filled = datetime.now()
+        new_order.filled = datetime.now()
+        match_order.counterparty_id = new_order.id
+        new_order.counterparty_id = match_order.id
+        session.commit()
+
+
+        # If match_order is not completely filled
+        if (new_order.sell_amount < match_order.buy_amount):
+            diff = match_order.buy_amount - new_order.sell_amount
+            exchange_rate_match = match_order.sell_amount / match_order.buy_amount
+            sell_amount_new_match = diff * exchange_rate_match
+
+            new_order = Order(sender_pk=match_order.sender_pk,
+                              receiver_pk=match_order.receiver_pk,
+                              buy_currency=match_order.buy_currency,
+                              sell_currency=match_order.sell_currency,
+                              buy_amount=diff,
+                              sell_amount=sell_amount_new_match,
+                              creator_id=match_order.id)
+            session.add(new_order)
+            session.commit()
     
-            if old_order.buy_amount > new_order.sell_amount:
-                new = Order(sender_pk = old_order.sender_pk,receiver_pk = old_order.receiver_pk, 
-                                  buy_currency =old_order.buy_currency, sell_currency = old_order.sell_currency, 
-                                  buy_amount = old_order.buy_amount - new_order.sell_amount, 
-                                  sell_amount= (old_order.buy_amount - new_order.sell_amount) * old_order.sell_amount / old_order.buy_amount,
-                                  creator_id = old_order.id)
-                session.add(new)
-                session.commit()
-            break
+
+        # If current_order is not completely filled
+        if (new_order.buy_amount > match_order.sell_amount):
+ 
+            diff = new_order.buy_amount - match_order.sell_amount
+            exchange_rate_current = new_order.buy_amount / new_order.sell_amount
+            sell_amount_new_current = diff / exchange_rate_current
+
+            new_order = Order(sender_pk=new_order.sender_pk,
+                              receiver_pk=new_order.receiver_pk,
+                              buy_currency=new_order.buy_currency,
+                              sell_currency=new_order.sell_currency,
+                              buy_amount=diff,
+                              sell_amount=sell_amount_new_current,
+                              creator_id=new_order.id)
+            session.add(new_order)
+            session.commit()
+        break
