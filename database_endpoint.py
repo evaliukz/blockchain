388ac1d151dd8d6d174a8896c77ec10c29e8d899
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-04-09 21:27:36 -0700

    Add files via upload

--- database_endpoint.py
+++ database_endpoint.py
@@ -10,73 +10,91 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import load_only
 
 from models import Base, Order, Log
+
 engine = create_engine('sqlite:///orders.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
+# database_endpoint.py
+
 app = Flask(__name__)
 
-#These decorators allow you to use g.session to access the database inside the request code
+
+# These decorators allow you to use g.session to access the database inside the request code
 @app.before_request
 def create_session():
-    g.session = scoped_session(DBSession) #g is an "application global" https://flask.palletsprojects.com/en/1.1.x/api/#application-globals
+    g.session = scoped_session(
+        DBSession)  # g is an "application global" https://flask.palletsprojects.com/en/1.1.x/api/#application-globals
+
 
 @app.teardown_appcontext
-def shutdown_session(response_or_exc):
+# def shutdown_session(response_or_exc):
+def shutdown_session(exception=None):
     g.session.commit()
     g.session.remove()
+
 
 """
 -------- Helper methods (feel free to add your own!) -------
 """
 
-def log_message(d)
-    # Takes input dictionary d and writes it to the Log table
+# Takes input dictionary d and writes it to the Log table
+# actually should be the JSON of the dict, not the dict
+# original requirement was wrong
+def log_message(d):
     create_session()
     order_obj = Log(message=d)
     g.session.add(order_obj)
     shutdown_session()
 
+    
 # convert a row in DB into a dict
-def convertRowToDict(row):
+def row2dict(row):
     return {
         c.name: getattr(row, c.name)
         for c in row.__table__.columns
     }
+    
 
 
 """
 ---------------- Endpoints ----------------
 """
-    
+
+
 @app.route('/trade', methods=['POST'])
 def trade():
     if request.method == "POST":
+        print("--------- trade ---------")
         content = request.get_json(silent=True)
-        print( f"content = {json.dumps(content)}" )
-        columns = [ "sender_pk", "receiver_pk", "buy_currency", "sell_currency", "buy_amount", "sell_amount", "platform" ]
-        fields = [ "sig", "payload" ]
+        print(f"content = {json.dumps(content)}")
+        columns = ["sender_pk", "receiver_pk", "buy_currency", "sell_currency", "buy_amount", "sell_amount", "platform"]
+        fields = ["sig", "payload"]
+
+        # check whether the input contains both "sig" and "payload"
         error = False
         for field in fields:
             if not field in content.keys():
-                print( f"{field} not received by Trade" )
-                print( json.dumps(content) )
+                print(f"{field} not received by Trade")
+                print(json.dumps(content))
                 log_message(content)
-                return jsonify( False )
-        
+                return jsonify(False)
+
+        # check whether the input contains all 7 fields of payload
         error = False
         for column in columns:
             if not column in content['payload'].keys():
-                print( f"{column} not received by Trade" )
+                print(f"{column} not received by Trade")
                 error = True
         if error:
-            print( json.dumps(content) )
+            print(json.dumps(content))
             log_message(content)
-            return jsonify( False )
-            
-        #Your code here
-        #Note that you can access the database session using g.session
-        #Get json contents
+            return jsonify(False)
+
+        # Your code here
+        # Note that you can access the database session using g.session
+
+        # extract contents from json
         sig = content['sig']
         payload = content['payload']
         pk = content['payload']['sender_pk']
@@ -86,26 +104,25 @@ def trade():
         # The platform must be either “Algorand” or "Ethereum".
         platforms = ["Algorand", "Ethereum"]
         if not platform in platforms:
-            print("platform is not Algorand or Ethereum")
+            print("input platform is not Algorand or Ethereum")
             return jsonify(False)
 
-        #The platform must be either “Algorand” or "Ethereum". 
-        #Your code should check whether “sig” is a valid signature of json.dumps(payload), 
-        #using the signature algorithm specified by the platform field. 
-        #Be sure to verify the payload using the sender_pk.
+        # check whether “sig” is a valid signature of json.dumps(payload),
+        # using the signature algorithm specified by the platform field.
+        # Be sure to verify the payload using the sender_pk.
         result = False
 
         if platform == "Algorand":
-            #print("platform is Algorand")
+            print("Algorand")
             if algosdk.util.verify_bytes(payload_json.encode('utf-8'), sig, pk):
-                print("Algorand sig verified!")
+                print("Algo sig verifies!")
                 result = True
 
         elif platform == "Ethereum":
-            #print("platform is Ethereum")
+            print("Ethereum")
             eth_encoded_msg = eth_account.messages.encode_defunct(text=payload_json)
             if eth_account.Account.recover_message(eth_encoded_msg, signature=sig) == pk:
-                print("Ethereum sig verified!")
+                print("Eth sig verifies!")
                 result = True
         
         # In this assignment, you will not need to match or fill the orders,
@@ -114,7 +131,7 @@ def trade():
         # If the signature verifies, store the signature,
         # as well as all of the fields under the ‘payload’ in the “Order” table EXCEPT for 'platform’.
         if result is True:
-            print("signature verified")
+            print("signature verifies")
             create_session()
             order_obj = Order(sender_pk=payload['sender_pk'],
                               receiver_pk=payload['receiver_pk'],
@@ -130,16 +147,17 @@ def trade():
         # If the signature does not verify, do not insert the order into the “Order” table.
         # Instead, insert a record into the “Log” table, with the message field set to be json.dumps(payload).
         if result is False:
-            print("signature was not verified")
+            print("signature does NOT verify")
             log_message(payload_json)            
             return jsonify(result)
 
-
+        
 @app.route('/order_book')
 def order_book():
-    #Your code here
-    #Note that you can access the database session using g.session
-        # The “/order_book” endpoint should return a list of all orders in the database.
+    # Your code here
+    # Note that you can access the database session using g.session
+
+    # The “/order_book” endpoint should return a list of all orders in the database.
     # The response should contain a single key “data” that refers to a list of orders formatted as JSON.
     # Each order should be a dict with (at least) the following fields
     # ("sender_pk", "receiver_pk", "buy_currency", "sell_currency", "buy_amount", "sell_amount", “signature”).
@@ -148,7 +166,7 @@ def order_book():
         
     # get orders from DB into a list
     order_dict_list = [
-           convertRowToDict(order)
+           row2dict(order)
            for order in g.session.query(Order).all()
     ]
         
@@ -164,6 +182,7 @@ def order_book():
 
     shutdown_session()
     return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(port='5002')
