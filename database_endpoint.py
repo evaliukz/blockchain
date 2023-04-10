360294333fcfa569b4fa03b9f4e2d115430a5cf1
Eva Liu <49618470+evaliukz@users.noreply.github.com>
2023-04-09 21:09:55 -0700

    Add files via upload

--- database_endpoint.py
+++ database_endpoint.py
@@ -65,6 +65,64 @@ def trade():
             
         #Your code here
         #Note that you can access the database session using g.session
+        #Get json contents
+        sig = content['sig']
+        payload = content['payload']
+        pk = content['payload']['sender_pk']
+        platform = content['payload']['platform']
+        payload_json = json.dumps(payload)
+
+        # The platform must be either “Algorand” or "Ethereum".
+        platforms = ["Algorand", "Ethereum"]
+        if not platform in platforms:
+            print("platform is not Algorand or Ethereum")
+            return jsonify(False)
+
+        #The platform must be either “Algorand” or "Ethereum". 
+        #Your code should check whether “sig” is a valid signature of json.dumps(payload), 
+        #using the signature algorithm specified by the platform field. 
+        #Be sure to verify the payload using the sender_pk.
+        result = False
+
+        if platform == "Algorand":
+            #print("platform is Algorand")
+            if algosdk.util.verify_bytes(payload_json.encode('utf-8'), sig, pk):
+                print("Algorand sig verified!")
+                result = True
+
+        elif platform == "Ethereum":
+            #print("platform is Ethereum")
+            eth_encoded_msg = eth_account.messages.encode_defunct(text=payload_json)
+            if eth_account.Account.recover_message(eth_encoded_msg, signature=sig) == pk:
+                print("Ethereum sig verified!")
+                result = True
+        
+        # In this assignment, you will not need to match or fill the orders,
+        # simply insert them into the database (if the signature verifies).
+
+        # If the signature verifies, store the signature,
+        # as well as all of the fields under the ‘payload’ in the “Order” table EXCEPT for 'platform’.
+        if result is True:
+            print("signature verified")
+            create_session()
+            order_obj = Order(sender_pk=payload['sender_pk'],
+                              receiver_pk=payload['receiver_pk'],
+                              buy_currency=payload['buy_currency'],
+                              sell_currency=payload['sell_currency'],
+                              buy_amount=payload['buy_amount'],
+                              sell_amount=payload['sell_amount'],
+                              signature=sig)            
+            g.session.add(order_obj)
+            shutdown_session()
+            return jsonify(result)
+
+        # If the signature does not verify, do not insert the order into the “Order” table.
+        # Instead, insert a record into the “Log” table, with the message field set to be json.dumps(payload).
+        if result is False:
+            print("signature was not verified")
+            log_message(payload_json)            
+            return jsonify(result)
+
 
 @app.route('/order_book')
 def order_book():
